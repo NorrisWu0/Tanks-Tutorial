627e978e3d55aab6d7ad1b30e8b49be0f5a71ffe
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2020-10-31 20:07:39 +1100

    Added tank firing function
    tank can now charge and fire shell in desired direction.

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -4,17 +4,16 @@ using UnityEngine.UI;
 public class TankShooting : MonoBehaviour
 {
     public int m_PlayerNumber = 1;       
-    public Rigidbody m_Shell;            
-    public Transform m_FireTransform;    
-    public Slider m_AimSlider;           
-    public AudioSource m_ShootingAudio;  
-    public AudioClip m_ChargingClip;     
-    public AudioClip m_FireClip;         
-    public float m_MinLaunchForce = 15f; 
-    public float m_MaxLaunchForce = 30f; 
-    public float m_MaxChargeTime = 0.75f;
+    [SerializeField] Rigidbody m_Shell = null;            
+    [SerializeField] Transform m_Muzzle = null;    
+    [SerializeField] Slider m_AimSlider = null;           
+    [SerializeField] AudioSource m_ShootingAudio = null;  
+    [SerializeField] AudioClip m_ChargingClip = null;     
+    [SerializeField] AudioClip m_FireClip = null;         
+    [SerializeField] float m_MinLaunchForce = 15f; 
+    [SerializeField] float m_MaxLaunchForce = 30f; 
+    [SerializeField] float m_MaxChargeTime = 0.75f;
 
-    /*
     private string m_FireButton;         
     private float m_CurrentLaunchForce;  
     private float m_ChargeSpeed;         
@@ -34,16 +33,58 @@ public class TankShooting : MonoBehaviour
 
         m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
     }
-    */
 
+    /// <summary>
+    /// Track the current state of the fire button and make decisions based on the current launch force.
+    /// </summary>
     private void Update()
     {
-        // Track the current state of the fire button and make decisions based on the current launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            // Max charge reached, fire shell.
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+
+        }
+        else if (Input.GetButtonDown(m_FireButton))
+        {
+            // Pressed fire button, start charging from mininum launch force.
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            // Holding fire button to charge launch force.
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            // Released button and fire shell.
+            Fire();
+        }
+
     }
 
-
+    /// <summary>
+    /// Instantiate and launch the shell.
+    /// </summary>
     private void Fire()
     {
-        // Instantiate and launch the shell.
+        m_Fired = true;
+
+        Rigidbody _shellRigidbody = Instantiate(m_Shell, m_Muzzle.position, m_Muzzle.rotation) as Rigidbody;
+        _shellRigidbody.velocity = m_CurrentLaunchForce * m_Muzzle.forward;
+
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+
+        m_CurrentLaunchForce = m_MinLaunchForce;
     }
 }
